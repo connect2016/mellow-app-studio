839f97cb743cd68fd8f6e78fe875014afa2e8e07
delasol94 <delasol94@gmail.com>
2026-07-07 15:09:38 -0500

    FanMap: CARTO Dark Matter neon basemap + proper attribution

--- src/pages/FanMap.tsx
+++ src/pages/FanMap.tsx
@@ -137,8 +137,8 @@ export default function FanMap() {
           attributionControl={false}
         >
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; OSM'
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; OpenStreetMap &copy; CARTO'
           />
           <FitToFans positions={allPositions} />
           {fansWithGate.map((fan) => {

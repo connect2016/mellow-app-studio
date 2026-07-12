0e99df27617c32c2523774b5454dec3eb89a7122
delasol94 <delasol94@gmail.com>
2026-07-12 16:09:18 -0500

    Hide Report/Block menu from guests on public profile

--- src/pages/PublicProfile.tsx
+++ src/pages/PublicProfile.tsx
@@ -218,7 +218,7 @@ export default function PublicProfile() {
           <ArrowLeft className="h-5 w-5" />
         </button>
 
-        {!isOwn && (
+        {user && !isOwn && (
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <button
